116928c7e6bcc427d24b453c661c392734b6c7c7
CACruz <cruz1706@gmail.com>
2016-10-09 14:48:58 -0700

    Class 14
    I had to upload this file because the terminal crashed and would not allow me to add the file in the normal process.

---
+++ class-14-managing-state/pair-assignment/CruzCLS14/scripts/articleController.js
@@ -0,0 +1,61 @@
+(function(module) {
+  var articlesController = {};
+
+  Article.createTable();  // Ensure the database table is properly initialized
+
+  articlesController.index = function(ctx, next) {
+    articleView.index(ctx.articles);
+  };
+
+  // COMMENT: What does this method do?    ANS: Get articles,attaches the article to the function, then calls next function
+  //What is it's execution path?           ANS: calls middleware when user tries to go to specific article
+  articlesController.loadById = function(ctx, next) {
+    var articleData = function(article) {
+      ctx.articles = article;
+      next();
+    };
+
+    Article.findWhere('id', ctx.params.id, articleData);
+  };
+
+  // COMMENT: What does this method do?   ANS: Gets author,attaches the author to the function, then calls next function
+  //What is it's execution path?          ANS: calls middleware when user tries to go to specific author
+  articlesController.loadByAuthor = function(ctx, next) {
+    var authorData = function(articlesByAuthor) {
+      ctx.articles = articlesByAuthor;
+      next();
+    };
+
+    Article.findWhere('author', ctx.params.authorName.replace('+', ' '), authorData);
+  };
+
+  // COMMENT: What does this method do?  ANS: Gets category data,attaches the category data to the function, then calls next function
+  //What is it's execution path?         ANS: calls middleware when user tries to go to specific category data
+  articlesController.loadByCategory = function(ctx, next) {
+    var categoryData = function(articlesInCategory) {
+      ctx.articles = articlesInCategory;
+      next();
+    };
+
+    Article.findWhere('category', ctx.params.categoryName, categoryData);
+  };
+
+  // COMMENT: What does this method do?  ANS: Gets article data,attaches the article data to the function, then calls next function
+  //What is it's execution path?         ANS: calls middleware when user tries to go to specific artricle data
+  articlesController.loadAll = function(ctx, next) {
+    var articleData = function(allArticles) {
+      ctx.articles = Article.all;
+      next();
+    };
+
+    if (Article.all.length) {
+      ctx.articles = Article.all;
+      next();
+    } else {
+      Article.fetchAll(articleData);
+    }
+  };
+
+
+  module.articlesController = articlesController;
+})(window);
